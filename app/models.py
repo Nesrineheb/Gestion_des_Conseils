9a4f73e39b0c9fa6228b1fb4ad899f31278ccd17
hebbadjnesrine <hn_hebbadj@esi.dz>
2021-06-15 14:55:10 +0100

    Authentification/CreationCompte/Menu

---
+++ app/models.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Logistique(models.Model):
+    """
+    La représentation d'un projet dans le système.
+    Un projet est pris en charge par une équipe.
+    """
+    DateReunion= models.DateField
+    SalleReunion= models.CharField(max_length=150)
+    Transport= models.CharField(max_length=150)
+    Nouriture= models.CharField(max_length=150)
