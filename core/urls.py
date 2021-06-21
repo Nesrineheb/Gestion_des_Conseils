ff03f360d1caef1d35208861354251898654651c
BouguesriRania <hr_bouguesri@esi.dz>
2021-06-21 12:09:13 +0100

    pvcs not complete

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("authentication.urls")), # Auth routes - login / register
     path("", include("app.urls"))             # UI Kits Html files
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
